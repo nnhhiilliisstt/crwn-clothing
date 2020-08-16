265de24ddff717fc7c8d41af5d3de3a9ba69b065
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-08-16 05:36:00 +0530

    router hoc

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,14 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div className={`${size} menu-item`} 
-  onClick={ () => history.push(`${match.url}${linkUrl}`) }>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
+  <div 
+    className={`${size} menu-item`} 
+    onClick={ () => history.push(`${match.url}${linkUrl}`) }
+   >
+     <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
       }}
     />
     <div className='content'>
