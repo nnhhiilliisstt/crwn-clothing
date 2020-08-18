2f72b82cd0ef03630fae507d5bc02f1e0ee47448
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-08-19 05:11:01 +0530

    added signin form custom button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Header from './components/header/header.component';
+
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -17,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
