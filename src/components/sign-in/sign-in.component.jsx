acbc042abe6270f6adee4dfb2beefb3f5bf3c677
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-08-19 21:59:26 +0530

    indentatation fix

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -35,27 +35,27 @@ handleChange = event =>{
 
     render(){
         return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>SignIn with your email and password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                        name="email"
-                        type="email" 
-                        value={this.state.email} 
-                        handleChange={this.handleChange}
-                        label='email'
-                        required 
-                     />
-                
-                    <FormInput 
-                        name="password" 
-                        type="password" 
-                        value={this.state.password} 
-                        handleChange={this.handleChange}
-                        label='password'
-                        required 
-                    />
+                <div className='sign-in'>
+                    <h2>I already have an account</h2>
+                    <span>SignIn with your email and password</span>
+                    <form onSubmit={this.handleSubmit}>
+                        <FormInput 
+                            name="email"
+                            type="email" 
+                            value={this.state.email} 
+                            handleChange={this.handleChange}
+                            label='email'
+                            required 
+                        />
+                    
+                        <FormInput 
+                            name="password" 
+                            type="password" 
+                            value={this.state.password} 
+                            handleChange={this.handleChange}
+                            label='password'
+                            required 
+                        />
                     
 
                     <CustomButton type='submit'> SignIn</CustomButton>
